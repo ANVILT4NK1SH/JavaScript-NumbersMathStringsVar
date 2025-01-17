a591b48f1c18ccaec93d9266c18dd0d40af973f1
Shawn <iamshawnhartline@gmail.com>
2025-01-16 20:40:39 -0600

    finished

--- jsVarEx1/script.js
+++ jsVarEx1/script.js
@@ -7,12 +7,26 @@ let firstName = document.getElementById('firstName').value;
 
 let lastName = document.getElementById('lastName').value;
 
-document.getElementById("welcomeMessage").innerHTML = "Welcome" + firstName + " " + lastName + "!"
+document.getElementById("welcomeMessage").innerHTML = "Welcome " + firstName + " " + lastName + "!"
 }
 
 const price1 = 29.95
 console.log('$' + (((price1* 0.07) + price1 )).toFixed(2))
 
+let inventory = 100
+inventory -= 15
+inventory += 20
+console.log(inventory)
 
+const originalPrice = 120.00;
+const tax = 0.1;
+const discount = 0.15;
 
+ const priceWithTax = (originalPrice * tax + originalPrice).toFixed(2);
 
+ const discountPrice = (originalPrice - (originalPrice * discount));
+
+const discountTax = parseFloat(discountPrice+ discountPrice * tax).toFixed(2);
+
+console.log(`With Tax: $${priceWithTax}`)
+console.log(`After Discount: $${discountTax}`)
