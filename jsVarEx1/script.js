af318f0f4ad506a9e7667078e3b9a1f0ffa0d19a
Shawn <iamshawnhartline@gmail.com>
2025-01-16 19:10:18 -0600

    first commit

---
+++ jsVarEx1/script.js
@@ -0,0 +1,18 @@
+console.log (5+3)
+
+console.log (5-3)
+
+function greeting(){
+let firstName = document.getElementById('firstName').value;
+
+let lastName = document.getElementById('lastName').value;
+
+document.getElementById("welcomeMessage").innerHTML = "Welcome" + firstName + " " + lastName + "!"
+}
+
+const price1 = 29.95
+console.log('$' + (((price1* 0.07) + price1 )).toFixed(2))
+
+
+
+
